15c6d8d45f3f5fb30ebe34adffe20fff0314b129
ezequiel <ezequiel.alganiaraz@gmail.com>
2020-10-09 19:33:48 -0300

    docs agregado

---
+++ docs/precache-manifest.8124cf465d9098f4f102463cf5fa5205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1b0898281dd4b7cae4efbc3709a13f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "626627007797d21f9938",
+    "url": "/static/css/main.87773d73.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626627007797d21f9938",
+    "url": "/static/js/main.a25c1b65.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
